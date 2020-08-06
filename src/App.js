f03e51bfa67c7b16a71319725492ee36940e6a86
Markus Weis <markus.weis1@web.de>
2020-08-06 14:48:15 +0200

    blockchain backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [chain, setChain] = useState({});
+
+  useEffect(() => {
+    fetch('/chain').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>The whole chain looks like this:</p>
+        {for(var i in chain)}
       </header>
     </div>
   );
